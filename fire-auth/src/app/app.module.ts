8eb9cdf8d420577106287f1834e5df8f40e013f8
Igor Ovcharenko <igor.ovcharenko@p2h.local>
2019-07-01 15:36:03 +0300

    Set firebase environment.

--- fire-auth/src/app/app.module.ts
+++ fire-auth/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -10,7 +15,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- fire-auth/src/environments/environment.ts
+++ fire-auth/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB22AVuDYS6KVsYmC6XpF7PeaproZaUfwU",
+    authDomain: "authentification-f3fb8.firebaseapp.com",
+    databaseURL: "https://authentification-f3fb8.firebaseio.com",
+    projectId: "authentification-f3fb8",
+    storageBucket: "authentification-f3fb8.appspot.com",
+    messagingSenderId: "959905424326"
+  }
 };
 
 /*
